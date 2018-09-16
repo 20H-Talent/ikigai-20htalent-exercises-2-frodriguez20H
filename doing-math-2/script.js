78de7cc30ce9bec0175687ecb2eea62a435c054b
Fabián <frodriguez@20H.es>
2018-09-16 11:49:09 +0100

    Create one last variable name profitPerUnit. In that variable store the amount of profit you made for each unit. You can calculate this by dividing the profit by the quantity.

--- doing-math-2/script.js
+++ doing-math-2/script.js
@@ -4,3 +4,8 @@ var quantity = 47;
 
 var salesTotal = retailPrice * quantity;
 var profit = salesTotal - (wholesalePrice * quantity);
+var profitPerUnit = profit / quantity;
+
+console.log("Sales Total: " + salesTotal);
+console.log("Profit = " + profit);
+console.log("Profit per unit = " + profitPerUnit);
