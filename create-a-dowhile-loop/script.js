da8ad4d8b0829b838f0e44fa4e567d0124299a30
Fabián <frodriguez@20H.es>
2018-09-20 12:09:27 +0100

    This is the code we used in the last code challenge. After learning about do...while loops, don't you think this would work better in the do...while style?  Re-write the code to use a do...while loop.

--- create-a-dowhile-loop/script.js
+++ create-a-dowhile-loop/script.js
@@ -1,5 +1,4 @@
 var secret = prompt("What is the secret password?");
-while ( secret !== "sesame" ) {
-  secret = prompt("What is the secret password?");    
-}
+do {secret = prompt("What is the secret password?");    
+} while ( secret !== "sesame" );
 document.write("You know the secret password. Welcome.");
